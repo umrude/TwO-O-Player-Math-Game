fe142e1f2f5598214bf84385d60276306cecceda
Gabi Geollegue <gabigeollegue@gmail.com>
2019-11-11 19:52:17 +0000

    init commit

---
+++ math_game_plan.rb
@@ -0,0 +1,11 @@
+# score_p1, score_p2 lives_p1, lives_p2, question
+# classes
+# player
+    # lives remaining
+    # current score
+
+# questions
+    # correct:
+            # add to score   
+    # wrong:
+            # subtract lives
