c039f3d9844e2c896c8af05eedda79d55c24927c
Gabi Geollegue <gabigeollegue@gmail.com>
2019-11-11 19:55:24 +0000

    add files for classes

---
+++ main.rb
@@ -0,0 +1,17 @@
+# game
+    # init
+    # start with player 1
+    # call player class
+        # player1/player2
+    
+    # def turn
+        # ask a question from class questions
+        # check_question
+        # end turn
+        # switch player
+
+    # def check_question
+        # check which player is currently playing
+        # subtract lives/add score from current player
+        # check how many lives left
+        #   if lives are 0, end game
---
+++ questions.rb
@@ -0,0 +1,11 @@
+# questions
+    # init
+        # random question
+
+    #def ask_question
+        # get player's answer
+
+        # if answer is right
+        # lives stay the same
+        # else
+        # lives--
